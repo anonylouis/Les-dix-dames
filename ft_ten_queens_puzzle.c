dedde545296674b15dc5ab548d0d3ff23a11dedc
Louis CALVIE <lcalvie@e3r3p15.clusters.42paris.fr>
2022-10-11 15:50:27 +0200

    return void to int

--- ft_ten_queens_puzzle.c
+++ ft_ten_queens_puzzle.c
@@ -6,13 +6,13 @@
 /*   By: lcalvie <lcalvie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 15:19:02 by lcalvie           #+#    #+#             */
-/*   Updated: 2022/10/11 15:41:16 by lcalvie          ###   ########.fr       */
+/*   Updated: 2022/10/11 15:50:10 by lcalvie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_ten_queens_puzzle_rec(char t[11], int n, int *count, int i)
+int	ft_ten_queens_puzzle_rec(char t[11], int n, int *count, int i)
 {
 	int	j;
 
@@ -21,13 +21,14 @@ void	ft_ten_queens_puzzle_rec(char t[11], int n, int *count, int i)
 		j = i;
 		while (++j < n)
 			if (t[i] == t[j] || t[j] == t[i] + j - i || t[j] == t[i] - j + i)
-				return ;
+				return (*count);
 	}
 	if (n > 9)
-		return (write(1, t, 11), (*count)++, (void)0);
+		return (write(1, t, 11), (*count)++, *count);
 	t[n] = '/';
 	while ((++t[n]) <= '9')
 		ft_ten_queens_puzzle_rec(t, n + 1, count, -1);
+	return (*count);
 }
 
 int	ft_ten_queens_puzzle(void)
@@ -37,6 +38,5 @@ int	ft_ten_queens_puzzle(void)
 
 	count = 0;
 	t[10] = '\n';
-	ft_ten_queens_puzzle_rec(t, 0, &count, -1);
-	return (count);
+	return (ft_ten_queens_puzzle_rec(t, 0, &count, -1));
 }
