d9751bb1559254470780bc4db514b7f2c985539e
Louis CALVIE <lcalvie@e3r3p15.clusters.42paris.fr>
2022-10-11 15:20:01 +0200

    minimum lines to beat

---
+++ ft_ten_queens_puzzle.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ten_queens_puzzle.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcalvie <lcalvie@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/11 15:19:02 by lcalvie           #+#    #+#             */
+/*   Updated: 2022/10/11 15:19:13 by lcalvie          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_ten_queens_puzzle_rec(char t[11], int n, int *count, int i)
+{
+	int	j;
+
+	while (++i < n)
+	{
+		j = i;
+		while (++j < n)
+			if (t[i] == t[j] || t[j] == t[i] + j - i || t[j] == t[i] - j + i)
+				return ;
+	}
+	if (n > 9)
+		return (write(1, t, 11), (*count)++, (void)0);
+	i = -1;
+	while (++i < 10)
+	{
+		t[n] = i + '0';
+		ft_ten_queens_puzzle_rec(t, n + 1, count, -1);
+	}
+}
+
+int	ft_ten_queens_puzzle(void)
+{
+	char	t[11];
+	int		count;
+
+	count = 0;
+	t[10] = '\n';
+	ft_ten_queens_puzzle_rec(t, 0, &count, -1);
+	return (count);
+}
