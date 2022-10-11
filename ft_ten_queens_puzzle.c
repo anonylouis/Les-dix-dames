1fae66af41fac11b17066a0b1b6e437883fca4e7
Louis CALVIE <lcalvie@e3r3p15.clusters.42paris.fr>
2022-10-11 15:42:01 +0200

    removing i variable

--- ft_ten_queens_puzzle.c
+++ ft_ten_queens_puzzle.c
@@ -6,7 +6,7 @@
 /*   By: lcalvie <lcalvie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 15:19:02 by lcalvie           #+#    #+#             */
-/*   Updated: 2022/10/11 15:23:02 by lcalvie          ###   ########.fr       */
+/*   Updated: 2022/10/11 15:41:16 by lcalvie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,9 @@ void	ft_ten_queens_puzzle_rec(char t[11], int n, int *count, int i)
 	}
 	if (n > 9)
 		return (write(1, t, 11), (*count)++, (void)0);
-	i = -1;
-	while (++i < 10)
-	{
-		t[n] = i + '0';
+	t[n] = '/';
+	while ((++t[n]) <= '9')
 		ft_ten_queens_puzzle_rec(t, n + 1, count, -1);
-	}
 }
 
 int	ft_ten_queens_puzzle(void)
